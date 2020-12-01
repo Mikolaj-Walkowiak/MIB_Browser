1161c19835d942880f0c85ea2f72b7a7a50faba0
Mikolaj-Walkowiak <mwalkowiak97@gmail.com>
2020-12-01 20:24:07 +0100

    .net 5 enabled

---
+++ MIB_Browser/MIB_Browser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MIB_Browser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
