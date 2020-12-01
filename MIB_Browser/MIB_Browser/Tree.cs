0adab7b4823f0c59a47fe91f2b04ee45d3704d10
Mikolaj-Walkowiak <mwalkowiak97@gmail.com>
2020-12-02 00:21:25 +0100

    Create Tree.cs

---
+++ MIB_Browser/MIB_Browser/Tree.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Class1
+{
+
+	struct Node {
+		int NodeNumber { get; }
+		string NodeName { get; }
+
+		string Syntax { get; }
+		string Access { get; }
+		string Status { get; }
+		string Description { get; }
+
+		public Node(int nodeNumber, string nodeName, string syntax, string access, string status, string description)
+		{
+			NodeNumber = nodeNumber;
+			NodeName = nodeName;
+			Syntax = syntax;
+			Access = access;
+			Status = status;
+			Description = description;
+		}
+	}
+	struct Tree{
+		Node root { get; }
+		public List<Node> children;
+
+
+
+
+        Tree()
+        {
+
+			children = new List<Node>();
+
+		}
+
+	
+	}
+	
+}
