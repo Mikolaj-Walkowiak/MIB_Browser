a8230a0088886484c54496508ab9ff77989bd391
jszymk <janusz.szymkowiak1@gmail.com>
2020-12-01 22:59:22 +0100

    regex

---
+++ MIB_Browser/MIB_Browser/Expressions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Expressions
+{
+
+	public const string br = "(?:\\s|--.*\\n\\s)+";
+
+	public static Regex objectType = new Regex("^" +
+									"(?<name>\\w*)" + br + "OBJECT-TYPE" + br + 
+										"(?:" +
+											"SYNTAX" + br + "(?<syntax>.+)" + br +					"|" +
+											"(?:ACCESS|MAX-ACCESS)" + br + "(?<access>.+)" + br +	"|" +
+											"STATUS" + br + "(?<status>.+)" + br +					"|" +
+											"DESCRIPTION" + br + "(?<desc>\"[^\"]*\")" + br +		"|" +
+										")*" +
+									"::=" + br + "(?<index>{[^}]*})"
+		);
+
+	public Expressions() {}
+}
--- MIB_Browser/MIB_Browser/Program.cs
+++ MIB_Browser/MIB_Browser/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace MIB_Browser
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = File.ReadAllText("inp.txt");
+            Console.WriteLine(input);
+            Match match = Expressions.objectType.Match(input);
+            
+        }
+
+        static string GetNamedGroup(Match match, string name)
+        {
+            foreach(string groupName in match.GetGroup)
         }
     }
 }
