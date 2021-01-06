f932da1cd42ce91b269b3d899400f3f1681ae934
Mikolaj-Walkowiak <mwalkowiak97@gmail.com>
2021-01-06 23:23:46 +0100

    Constraints

---
+++ MIB_Browser/MIB_Browser/Coder.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+var types = new Dictionary<String, int>()
+{
+    {"BOOLEAN",1 },
+    {"INTEGER",2 },
+    {"BIT STRING",3 },
+    {"OCTET STRING",4 },
+    {"OBJECT IDENTIFIER",6 },
+    {"ObjectDescriptor",7 },
+    {"EXTERNAL",8 },
+    {"INSTANCE OF",8 },
+    {"REAL",9 },
+    {"ENUMERATED",10 },
+    {"EMBEDDED PDV",11 },
+    {"UTF8String",12 },
+    {"RELATIVE-OID",13 },
+    {"SEQUENCE",16 },
+    {"SEQUENCE OF",16 },
+    {"SET",17 },
+    {"SET OF",17 },
+    {"NumericString",18 },
+    {"PrintableString",19 },
+    {"TeletexString",20 },
+    {"T61String",20 },
+    {"VideotexString",21 },
+    {"IA5String",22 },
+    {"UTCTime",23 },
+    {"GeneralizedTime",24},
+    {"GraphicString",25 },
+    {"VisibleString",26 },
+    {"ISO646String",26 },
+    {"GeneralString",27 },
+    {"UniversalString",28 },
+    {"CHARACTER STRING",29 },
+    {"BMPString",30 }
+};
+
+
+public class Coder
+{
+    public Boolean CheckConstraints(string type, string value)
+    {
+        Constraint toCheck = Types.GetConstraints(type);
+        if (toCheck.isSize)
+        {
+            if (value.Length < Int32.Parse(toCheck.min) || value.Length > Int32.Parse(toCheck.max))
+            {
+                return false;
+            }
+        }
+        else { 
+            if(Int32.Parse(value) < Int32.Parse(toCheck.min) || Int32.Parse(value) > Int32.Parse(toCheck.max))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+	
+}
---
+++ MIB_Browser/MIB_Browser/Types.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Constraint
+{
+	public Constraint(Boolean IsSize, string Min, string Max)
+	{
+		isSize = IsSize;
+		min = Min;
+		max = Max;
+	}
+
+	public Boolean isSize { get; }
+	public string min { get; }
+	public string max { get; }
+}
+
+public static class Types
+{
+	public static Dictionary<String, Constraint> types = new Dictionary<String, Constraint>();
+
+	public static void AddNew(String Name, Boolean IsSize, String Min, String Max)
+	{
+		types.Add(Name, new Constraint(IsSize, Min, Max));
+	}
+	public static Constraint GetConstraints(String Name)
+    {
+		return types[Name];
+    }
+}
