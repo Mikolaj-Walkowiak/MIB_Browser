5e5aea87a6511dc29c43f84d40cf8aa69d9ed7da
jszymk <janusz.szymkowiak1@gmail.com>
2021-01-07 00:07:00 +0100

    fix

--- MIB_Browser/MIB_Browser/Coder.cs
+++ MIB_Browser/MIB_Browser/Coder.cs
@@ -2,45 +2,43 @@
 using System.Collections.Generic;
 
 
-var types = new Dictionary<String, int>()
-{
-    {"BOOLEAN",1 },
-    {"INTEGER",2 },
-    {"BIT STRING",3 },
-    {"OCTET STRING",4 },
-    {"OBJECT IDENTIFIER",6 },
-    {"ObjectDescriptor",7 },
-    {"EXTERNAL",8 },
-    {"INSTANCE OF",8 },
-    {"REAL",9 },
-    {"ENUMERATED",10 },
-    {"EMBEDDED PDV",11 },
-    {"UTF8String",12 },
-    {"RELATIVE-OID",13 },
-    {"SEQUENCE",16 },
-    {"SEQUENCE OF",16 },
-    {"SET",17 },
-    {"SET OF",17 },
-    {"NumericString",18 },
-    {"PrintableString",19 },
-    {"TeletexString",20 },
-    {"T61String",20 },
-    {"VideotexString",21 },
-    {"IA5String",22 },
-    {"UTCTime",23 },
-    {"GeneralizedTime",24},
-    {"GraphicString",25 },
-    {"VisibleString",26 },
-    {"ISO646String",26 },
-    {"GeneralString",27 },
-    {"UniversalString",28 },
-    {"CHARACTER STRING",29 },
-    {"BMPString",30 }
-};
-
-
 public static class Coder
 {
+    private static Dictionary<String, int> types = new Dictionary<String, int>()
+    {
+        {"BOOLEAN",1 },
+        {"INTEGER",2 },
+        {"BIT STRING",3 },
+        {"OCTET STRING",4 },
+        {"OBJECT IDENTIFIER",6 },
+        {"ObjectDescriptor",7 },
+        {"EXTERNAL",8 },
+        {"INSTANCE OF",8 },
+        {"REAL",9 },
+        {"ENUMERATED",10 },
+        {"EMBEDDED PDV",11 },
+        {"UTF8String",12 },
+        {"RELATIVE-OID",13 },
+        {"SEQUENCE",16 },
+        {"SEQUENCE OF",16 },
+        {"SET",17 },
+        {"SET OF",17 },
+        {"NumericString",18 },
+        {"PrintableString",19 },
+        {"TeletexString",20 },
+        {"T61String",20 },
+        {"VideotexString",21 },
+        {"IA5String",22 },
+        {"UTCTime",23 },
+        {"GeneralizedTime",24},
+        {"GraphicString",25 },
+        {"VisibleString",26 },
+        {"ISO646String",26 },
+        {"GeneralString",27 },
+        {"UniversalString",28 },
+        {"CHARACTER STRING",29 },
+        {"BMPString",30 }
+    };
 
     public static string EncodeToBono(string value)
     {
