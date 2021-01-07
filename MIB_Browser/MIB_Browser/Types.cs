b1d6165a5aead71f5e538d829fe5d8d7518ab37a
Mikolaj-Walkowiak <mwalkowiak97@gmail.com>
2021-01-07 11:39:58 +0100

    Update Types.cs

--- MIB_Browser/MIB_Browser/Types.cs
+++ MIB_Browser/MIB_Browser/Types.cs
@@ -4,21 +4,23 @@ using System.Collections.Generic;
 public class Constraint
 {
 
-	public Constraint(Boolean IsSize, Boolean IsExplicit, string ParentType, string Min, string Max)
+	public Constraint(Boolean IsSize, Boolean IsExplicit, string ParentType, string Min, string Max, String Location)
 	{
 		isSize = IsSize;
 		isExplicit = IsExplicit;
 		parentType = ParentType;
 		min = Min;
 		max = Max;
+		location = Location;
 	}
-	public Constraint(Boolean IsSize, Boolean IsExplicit, string ParentType, string Singular)
+	public Constraint(Boolean IsSize, Boolean IsExplicit, string ParentType, string Singular, String Location)
 	{
 		isSize = IsSize;
 		isExplicit = IsExplicit;
 		parentType = ParentType;
 		min = Singular;
 		max = Singular;
+		location = Location;
 	}
 
 
@@ -27,13 +29,15 @@ public class Constraint
 	public string parentType { get; }
 	public string min { get; }
 	public string max { get; }
+	public string location { get; } // APLICATION 1
 }
+
 
 public static class Types
 {
 	public static Dictionary<String, Constraint> types = new Dictionary<String, Constraint>();
 
-	public static void AddNew(String Name, Boolean IsSize, Boolean IsExplicit, string ParentType, String Min, String Max)
+	public static void AddNew(String Name, Boolean IsSize, Boolean IsExplicit, string ParentType, String Min, String Max, String Location)
 	{
 		types.Add(Name, new Constraint(IsSize, IsExplicit, ParentType, Min, Max));
 	}
@@ -43,6 +47,6 @@ public static class Types
 		{
 			return types[Name];
 		}
-		return new Constraint(false,false,"negro", "-2137", "2137"); //shouldn't be possible
+		return new Constraint(false,false,"negro", "-2137", "2137",null); //shouldn't be possible
     }
 }
